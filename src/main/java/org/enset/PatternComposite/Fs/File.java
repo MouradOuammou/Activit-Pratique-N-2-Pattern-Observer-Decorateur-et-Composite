6e53e33b0b19f3183c4e211024550f4a200bcd9c
Mourad Ouammou <mouradouammou8@gmail.com>
2025-09-17 13:21:39 +0100

    Pattern Composite

---
+++ src/main/java/org/enset/PatternComposite/Fs/File.java
@@ -0,0 +1,13 @@
+package org.enset.PatternComposite.Fs;
+
+public class File extends Composante {
+
+    public File(String name) {
+        super(name);
+    }
+
+    @Override
+    public void afficher() {
+        System.out.println(getTab()+"- File => "+name);
+    }
+}
---
+++ src/main/java/org/enset/PatternComposite/Fs/Folder.java
@@ -0,0 +1,28 @@
+package org.enset.PatternComposite.Fs;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Folder extends Composante {
+    private List<Composante> composantes=new ArrayList<>();
+
+    public Folder(String name) {
+        super(name);
+    }
+
+    @Override
+    public void afficher() {
+        System.out.println(getTab()+"- Folder : "+name);
+        for (Composante c : composantes){
+            c.afficher();
+        }
+    }
+    public Composante add(Composante c){
+        this.composantes.add(c);
+        c.level = this.level +1;
+        return c;
+    }
+    public void remove(Composante composante){
+        composantes.remove(composante);
+    }
+}
